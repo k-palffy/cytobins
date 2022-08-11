dd26a8081d48867590e905f0d3760bc424d3361e
unknown <xy@OK-0033.okologia.mta.local>
2022-08-11 17:42:05 +0200

    minor changes of no importance

--- R/binning_channel.R
+++ R/binning_channel.R
@@ -1,16 +1,16 @@
 binning_channel <- function(channel_vec, bins) {
-  tot.counter <- 1
+  tot_counter <- 1
   bincounter <- 1
-  bin.index <- vector(length = length(channel_vec))
+  bin_index <- vector(length = length(channel_vec))
   while(tot.counter <= length(channel_vec)) {
-    while(channel_vec[tot.counter] < bins[bincounter,2]) {
-      bin.index[tot.counter] <- bincounter
-      tot.counter <- tot.counter + 1
-      if(tot.counter > length(channel_vec)) {
+    while(channel_vec[tot_counter] < bins[bincounter,2]) {
+      bin_index[tot_counter] <- bincounter
+      tot_counter <- tot_counter + 1
+      if(tot_counter > length(channel_vec)) {
         break
       }
     }
     bincounter <- bincounter +1
   }
-  return(bin.index)
+  return(bin_index)
 }
