2c9d5148ca8aa8829c1d1240f34f5059d53721a7
unknown <xy@OK-0033.okologia.mta.local>
2022-08-25 21:55:04 +0200

    Complementing cyto_binning() description and adding example

--- R/cyto_binning.R
+++ R/cyto_binning.R
@@ -1,23 +1,43 @@
-#' Binning flow cytometric data
+#' Flow cytometric data binning
+#'
+#' Divide the data ranges of two selected channels of a flow cytometric data set (a GatingHierarchy object) into `n` categories.
+#' The process results in a matrix with `n` x `n` compartments (bins) in the space defined by the selected channels,
+#' assigning each detected cell/event to a sepcific bin according to its relevant channel signals.
 #'
 #' @param gh a GatingHierarchy object.
 #' @param node name of the node (gate).
-#' @param channels number of the channels (columns) in the cytometric data matrix, a vector with 2 elements.
-#' @param xlimits lower and upper limits of the 1st channel, a vector with 2 elements.
-#' @param ylimits lower and upper limits of the 2nd channel, a vector with 2 elements.
+#' @param channels index of two channels (columns) in the cytometric data matrix, a vector with 2 elements.
+#' @param xlimits lower and upper limits of the 1st channel, a vector with 2 elements. Must be at least as low/high as the minimun/maximum value within the channel node.
+#' @param ylimits lower and upper limits of the 2nd channel, a vector with 2 elements. Must be at least as low/high as the minimun/maximum value within the channel node.
 #' @param binnumber number of bins (compartments) to be created along the channel axes.
 #' @param vectorize logical indicating whether the resulting binning matrix should be converted into a vector for easier comparison with other GatingHierarchy objects.
 #'
-#' @return A binning matrix and (optionally) its vectorized form.
+#' @return A binning matrix and (optionally) its vectorized form. In the case of both, a list with the matrix as the first and the vector as the second element.
 #' @export
 #'
 #' @examples
+#' library(flowCore)
+#' library(flowWorkspace)
+#'
+#' # loading flow cytometric data of a phytoplankton sample as a GatingSet object
+#' phyto_gs   <- load_gs(path = system.file("extdata", "phyto_flowcyt_sample", package = "cytobins"))
+#' # creating a separate GatingHierarchy object
+#' phyto_gh   <- phyto_gs[[1]]
+#'
+#' # data binning along the axes of optical channels FSC and FL3, with 64x64 bins
+#' # performed on a gated population ("chl") of phyto_gh
+#' phyto_bins <- cyto_binning(gh = phyto_gh, node = "chl", channels = c(1,9),
+#'                            xlimits = c(3,6), ylimits = c(3,7), binnumber = 64)
+#' # the generated binning matrix
+#' phyto_bins[[1]]
+#' # the generated binning vector
+#' phyto_bins[[2]]
 cyto_binning <- function(gh, node = "root", channels, xlimits, ylimits, binnumber, vectorize = TRUE) {
 
   xbins       <- create_bins(xlimits[1], xlimits[2], binnumber)
   ybins       <- create_bins(ylimits[1], ylimits[2], binnumber)
 
-  gh_temp     <- prepare_gh(gh, node, channels)
+  gh_temp     <- prepare_gh(gh, prepnode = node, channel = channels)
   gh_temp     <- gh_temp[order(gh_temp[,1]),]
   gh_temp[,3] <- binning_channel(gh_temp[,1], xbins)
   gh_temp     <- gh_temp[order(gh_temp[,2]),]
